e6f149e99b813935dab27610513fefd67dfe275e
Richard Shea <rshea@thecubagroup.com>
2022-07-28 13:11:23 +1200

    All functionality now in place (but only rudimentary styling)

--- react_only/src/components/todo.jsx
+++ react_only/src/components/todo.jsx
@@ -11,12 +11,28 @@ const DEFAULT_TODO_ITEMS = [
 const Todo = () => {
 
     const [todoitems, setTodoItems] = useState(DEFAULT_TODO_ITEMS);
+    const [inputTodoValue, setInputTodoValue] = useState('');
 
     const deleteTodoItem = id => setTodoItems([...todoitems.filter(i => i.id !== id)]);
 
     return(
         <div style={{color: "blue", padding:"1em", border:"solid"}} >
             <h2>List of things to do</h2>
+            <div style={{margin:"0.5em"}} >
+                <input
+                  type="text"
+                  value={inputTodoValue}
+                  onChange={e => setInputTodoValue(e.currentTarget.value)}
+                />
+                &nbsp;
+                <button
+                  onClick={e =>
+                    setTodoItems([...todoitems, { id: todoitems.length + 1, text: inputTodoValue }])
+                  }
+                >
+                  Add a Todo 
+                </button>
+            </div>
             <div className="todo__items">
                 {todoitems.map(t => (
                     <TodoItem key={t.id} id={t.id} text={t.text} onDelete={deleteTodoItem} />
